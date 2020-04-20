a025d828e08c2dcf53da7b8214a72d506003ed1d
Jack Skiles <jackskiles420@gmail.com>
2020-04-20 11:51:11 -0400

    finalized tipCalc.js

--- tipCalc.js
+++ tipCalc.js
@@ -1,10 +1,47 @@
 
 
 function tipAmount(bill, service) {
-    if (service === 'fair') {
-        total = (bill/15);
+    if (service === "good") {
+        total = (bill * .20);
+    }
+    else if (service === 'fair') {
+        total = (bill * .15);
+    }
+    else if (service === 'bad') {
+        total = (bill * .10);
     }
     return total;
 }
 
-console.log(tipAmount(100, 'fair'));
+console.log(tipAmount(150, 'bad'));
+
+    
+function totalAmount(bill, service) {
+    if (service === "good") {
+        total = (bill * .20);
+    }
+    else if (service === 'fair') {
+        total = (bill * .15);
+    }
+    else if (service === 'bad') {
+        total = (bill * .10);
+    }
+    return total + bill;
+}
+
+console.log(totalAmount(150, 'bad'));
+
+function splitAmount(bill, service, split) {
+    if (service === "good") {
+        total = (bill * .20);
+    }
+    else if (service === 'fair') {
+        total = (bill * .15);
+    }
+    else if (service === 'bad') {
+        total = (bill * .10);
+    }
+    return (total + bill) / split;
+}
+
+console.log(splitAmount(170, 'bad', 3))
